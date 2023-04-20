1a8081dc03bc9468643354e7a0a44f7488704c51
codedjay672 <emmajoel6456@gmail.com>
2023-04-21 00:16:07 +0100

    update interview folder

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+this module contains functions for computing
+a pascal triangle.
+
+"""
+
+def pascal_triangle(n):
+    """ this function computes a pascal triangle
+    the function returns a list of lists containing the
+    values of the pascal triangle"""
+
+    result = []
+    for i in range(1, n+1):
+        c = 1
+        myList = []
+        for j in range(1, i+1):
+            myList.append(c)
+            c = c * (i - j) // j
+        result.append(myList)
+    return result
