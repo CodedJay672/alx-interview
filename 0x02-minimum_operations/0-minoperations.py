86c39f89daef4094df3350b5666dd255d12f4ac6
Codedjay672 <emmajoel6456@gmail.com>
2023-05-05 07:50:45 +0100

    update minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+this module contains functions for minimum operations
+
+"""
+
+
+def minOperations(n):
+    """
+    this function finds the minimum number of operations
+    taken to acieve a numbered goal
+
+    """
+
+
+    if n <= 1:
+        return 0
+
+    for x in range(2, int((n/2)+1)):
+        if n % x == 0:
+            return minOperations(int(n / x)) + x
+
+    return n
