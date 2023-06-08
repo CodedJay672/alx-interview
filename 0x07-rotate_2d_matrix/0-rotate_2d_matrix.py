482f1df380216c0321830cd30bdce3ce93b29d1e
codedjay672 <emmajoel6456@gmail.com>
2023-06-08 16:40:37 +0100

    fix pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,6 +4,7 @@
 
 """
 
+
 def rotate_2d_matrix(matrix):
     """
       function that rotates a 2d matrix in-place
@@ -17,3 +18,5 @@ def rotate_2d_matrix(matrix):
             matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
             matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
             matrix[j][N - 1 - i] = temp
+            if j == 2:
+                print()
