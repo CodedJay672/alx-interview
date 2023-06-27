19205b8bbf0d6a644047158fb0e7dbba0132b62b
codedjay672 <emmajoel6456@gmail.com>
2023-06-27 23:13:22 +0100

    upload starwars api file

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+// import the request
+const request = require('request');
+
+// save command line argument
+const arg = process.argv[2];
+
+// request from the api and store result in response
+const url = 'https://swapi-api.alx-tools.com/api/films/' + arg;
+
+// request for the url
+request(url, async (error, response, body) => {
+  if (error)
+    console.log(error);
+
+  const characters = JSON.parse(body).characters;
+
+  for (character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
+        if (error)
+	  console.log(error);
+	console.log(JSON.parse(body).name);
+      });
+      resolve();
+    });
+  }
+});
