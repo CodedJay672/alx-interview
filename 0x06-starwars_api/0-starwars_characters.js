720271fa3f4d9840fb50e2fdea763bf507bebe11
codedjay672 <emmajoel6456@gmail.com>
2023-06-28 08:21:48 +0100

    print names of actors as is on the character object

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,27 +5,20 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(url, function (error, response, body) {
+request(url, async (error, response, body) => {
   if (error) {
     console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Invalid Status Code Returned:', response.statusCode);
-  } else {
-    const movie = JSON.parse(body);
-    const characters = movie.characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.error('Error:', error);
-        } else if (response.statusCode !== 200) {
-          console.error(
-            'Invalid Status Code Returned:',
-            response.statusCode
-          );
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
+  }
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+  for (let i = 0; i < characters.length; i += 1) {
+    await new Promise((resolve, reject) => {
+      request(characters[i], (error, response, body) => {
+	if (error) {
+          console.log(error);
+	}
+	console.log(JSON.parse(body).name);
+	resolve();
       });
     });
   }
